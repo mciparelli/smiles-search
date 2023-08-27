d4ad42e49c2c90e13c8f553cfc113b55a82420a3
mciparelli <mciparelli@gmail.com>
2023-08-26 22:16:02 -0300

    default to 10 results

--- utils/signals.js
+++ utils/signals.js
@@ -3,6 +3,6 @@ import { persistedSignal } from "./storage.ts";
 
 const requestsSignal = signal({});
 const abortControllersSignal = signal([]);
-const resultadosSignal = persistedSignal(20, "smiles:resultados");
+const resultadosSignal = persistedSignal(10, "smiles:resultados");
 
 export { abortControllersSignal, requestsSignal, resultadosSignal };
