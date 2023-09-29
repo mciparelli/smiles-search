09fd51335ab20be415addec4c77e16cb4b4bc6aa
mciparelli <mciparelli@gmail.com>
2023-09-29 18:13:07 -0300

    toggle origin destination

--- components/origin-destination-inputs.jsx
+++ components/origin-destination-inputs.jsx
@@ -1,7 +1,11 @@
+import { useRef } from 'preact/hooks';
 import { filtros } from "utils/flight.js";
+import { ArrowPathIcon } from 'icons';
 import RegionsDropdown from "./regions-dropdown.jsx";
 
 export default function OriginDestinationInputs({ defaults, searchType }) {
+  const originRef = useRef();
+  const destinationRef = useRef();
   return (
     <fieldset class="group flex gap-2 w-full">
       {["from-region-to-airport", "from-region-to-region"].includes(searchType)
@@ -23,6 +27,7 @@ export default function OriginDestinationInputs({ defaults, searchType }) {
             placeholder="Desde"
             maxLength={3}
             onInput={(ev) => ev.target.value = ev.target.value.toUpperCase()}
+            ref={originRef}
             value={defaults.originAirportCode ??
               filtros.defaults.originAirportCode}
           />
@@ -46,10 +51,16 @@ export default function OriginDestinationInputs({ defaults, searchType }) {
             placeholder="Hacia"
             maxLength={3}
             onInput={(ev) => ev.target.value = ev.target.value.toUpperCase()}
+            ref={destinationRef}
             value={defaults.destinationAirportCode}
             autoFocus
           />
         )}
+      {"airports" === searchType && <button type="button" class="px-2"><ArrowPathIcon class="h-5 w-5" aria-hidden="true" onClick={() => {
+        const currentOrigin = originRef.current.value;
+        originRef.current.value = destinationRef.current.value;
+        destinationRef.current.value = currentOrigin;
+      }} /></button>}
     </fieldset>
   );
 }
