6b526600eba645dc91dfe937f2d2d50476c5409c
mciparelli <mciparelli@gmail.com>
2023-11-20 19:10:49 -0300

    change seed

--- utils/seed.ts
+++ utils/seed.ts
@@ -1,19 +1,12 @@
-import defaultRegionsObject from "juani/data/regions.js";
 import { findFlightsForDate } from "utils/api.js";
 import { formatDate } from "utils/dates.js";
 
-const regions = Object.entries(defaultRegionsObject);
+const airportsToSeed = ["SCL", "LIM", "BOG", "BUE", "EZE", "MVD", "ASU", "SAO", "CUN", "PTY", "PUJ", "SJO", "AUA", "HAV", "MEX", "CHI", "NYC", "LAX", "DFW", "SFO", "LAS", "MIA", "MCO", "LIS", "MAD", "BCN", "PAR", "AMS", "ROM", "LON", "FRA"];
 
-export default async function seed(date) {
-  for (const regionFrom of regions) {
-    const [_regionFromName, airportsFrom] = regionFrom;
-    for (const regionTo of regions) {
-      const [_regionToName, airportsTo] = regionTo;
-      await findFlightsForDate({
-        from: airportsFrom,
-        to: airportsTo,
-        date: formatDate(date),
-      });
-    }
-  }
+export default function seed(date) {
+  return findFlightsForDate({
+    from: airportsToSeed,
+    to: airportsToSeed,
+    date: formatDate(date),
+  });
 }
