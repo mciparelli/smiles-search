8efac985d352adcf6bb4b863c458656c9b04ef19
Martín Ciparelli <mciparelli@gmail.com>
2025-06-04 14:58:50 -0300

    fonts in head

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,6 +11,13 @@ function Layout({ children }) {
 				<meta name="keywords" content="smiles, smiles-search, compras, smiles.com.ar, búsqueda, millas, ofertas" />
 				<meta name="author" content="Martín Ciparelli" />
 				<title>Smiles Search</title>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+				<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+					rel="stylesheet"
+				/>
 				<link rel="stylesheet" href="output.css" />
 				<link rel="icon" type="image/svg+xml" href="glass.svg" />
 
